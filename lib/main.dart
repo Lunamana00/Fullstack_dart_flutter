c833eedae5ee6f0595a2e9a1d8734428931e298e
Lunamana00 <kth000928@naver.com>
2024-07-05 01:30:19 +0900

    remove duplicated import
    중복 파일 import 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pg_login.dart';
-import 'pg_register.dart';
-import 'pg_main.dart';
 import 'route_controller.dart';
-import 'package:table_calendar/table_calendar.dart';
-
 
 void main() => runApp(MyApp());
 
